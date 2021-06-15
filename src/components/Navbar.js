2a3a99e9ae573f54e502bf9a9ced2ebac8b80738
WojciechLisCode <wlis58372+code@gmail.com>
2021-06-15 17:04:48 +0200

    basic navigation added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,23 @@
 import React from "react";
 
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export default function Navbar() {
   return (
     <div>
-      <h2>Navbar</h2>
+      <div>
+        <NavLink to="/">*Candles*</NavLink>
+        <NavLink to="/users">*Users*</NavLink>
+        <span>LOGO</span>
+        <NavLink to="/user/2">*My account*</NavLink>
+        <NavLink to="/login">*Login/Signup*</NavLink>
+      </div>
+      <div>
+        <p>Choose language:</p>
+        <select>
+          <option value="English">English</option>
+        </select>
+      </div>
     </div>
   );
 }
