42342205d3c07759f6a553a2fbed1cb5c162bc8e
WojciechLisCode <wlis58372+code@gmail.com>
2021-08-04 18:10:44 +0200

    inputs fixed

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -46,7 +46,7 @@ export default function Signup() {
           <p className="inputDesc m0">Email:</p>
           <input
             className="inputField"
-            type="text"
+            type="email"
             value={email}
             onChange={(e) => {
               setEmail(e.target.value);
@@ -57,7 +57,7 @@ export default function Signup() {
           <p className="inputDesc m0">Password:</p>
           <input
             className="inputField"
-            type="passord"
+            type="password"
             value={password}
             onChange={(e) => {
               setPassword(e.target.value);
